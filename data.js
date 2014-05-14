ee6a143a1dfc6702f29c03dcbbf6bb58905793a2
Franz Webersberger <franzwebersberger@users.noreply.github.com>
2014-05-14 07:52:23 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,9 @@
+var N = 63;
+var myImages = [43, 44];
+
+var cols = 10;
+var allImageUrls = [];
+for (i=0; i<N; i++) {
+	allImageUrls[i] = "http://www.promo2014.de/wm_sticker/desktop/img/sticker/" + (i+1) + ".jpg";
+}
+
